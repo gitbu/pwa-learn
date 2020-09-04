5ea5277bddecf9e472347f0d45a8c492941882f7
gitbu <1282185668@qq.com>
2020-09-04 08:00:31 +0800

    temp

--- sw.js
+++ sw.js
@@ -38,18 +38,18 @@ self.addEventListener('install', function(e) {
   );
 });
 
-// Fetching content using Service Worker
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-      console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then(function(response) {
-        return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: ' + e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
+// // Fetching content using Service Worker
+// self.addEventListener('fetch', function(e) {
+//   e.respondWith(
+//     caches.match(e.request).then(function(r) {
+//       console.log('[Service Worker] Fetching resource: '+e.request.url);
+//       return r || fetch(e.request).then(function(response) {
+//         return caches.open(cacheName).then(function(cache) {
+//           console.log('[Service Worker] Caching new resource: ' + e.request.url);
+//           cache.put(e.request, response.clone());
+//           return response;
+//         });
+//       });
+//     })
+//   );
+// });
